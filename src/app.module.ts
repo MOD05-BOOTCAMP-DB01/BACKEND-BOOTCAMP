ebf7b8a786b0bffc42b16a1dfd877b4ec921e92b
DorivalRamos <fulanodetal@exemplo.br>
2021-10-28 20:32:42 -0300

    Fix delete app Controller e Service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { AuthModule } from './auth/auth.module';
 import { ObjectivesModule } from './objectives/objectives.module';
 import { KeyResultsModule } from './key-results/key-results.module';
@@ -8,7 +7,7 @@ import { CheckinModule } from './checkin/checkin.module';
 
 @Module({
   imports: [AuthModule, ObjectivesModule, KeyResultsModule, CheckinModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
