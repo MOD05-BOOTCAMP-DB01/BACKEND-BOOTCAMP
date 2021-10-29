11f752ed8bd990c572a6ef1d42ef00e6406cff28
janicecaldeira <janicecaldeira@gmail.com>
2021-10-29 00:44:28 -0300

    fix: app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './configs/typeorm.config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { LoggerInterceptor } from '../src/interceptors/logger.interceptor';
+import { LoggerInterceptor } from 'src/interceptors/logger.interceptor';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { WinstonModule } from 'nest-winston';
 import { winstonConfig } from '../src/configs/winston.config';
