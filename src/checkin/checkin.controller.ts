79c2997a16db555bb2b6b17cd040244c1260710c
Priscila Miranda (fraser) <priscila.fraser@gmail.com>
2021-11-18 09:35:22 -0400

    Feat: swagger checkin

--- src/checkin/checkin.controller.ts
+++ src/checkin/checkin.controller.ts
@@ -20,13 +20,20 @@ import { UserRole } from '../users/user-roles.enum';
 import { CheckinService } from './checkin.service';
 import { GetUser } from 'src/auth/get-user.decorator';
 import { User } from 'src/users/user.entity';
+import { ApiCreatedResponse, ApiForbiddenResponse, ApiInternalServerErrorResponse, ApiNotFoundResponse, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Checkin')
 @Controller('checkin')
 @UseGuards(AuthGuard(), RolesGuard)
 export class CheckinController {
   constructor(private checkinService: CheckinService) {}
 
   @Post()
+  @ApiOperation(({summary: 'Cria checkins'}))
+  @ApiCreatedResponse({ description: 'Check-in cadastrado com sucesso' })
+  @ApiInternalServerErrorResponse({
+    description: 'Erro ao salvar o checkin no banco de dados',
+  })
   async createCheckin(
     @Body(ValidationPipe) createCheckinDto: CreateCheckinDto,
   ): Promise<ReturnCheckinDto> {
@@ -38,11 +45,16 @@ export class CheckinController {
   }
 
   @Get()
+  @ApiOperation({ summary: 'Busca todos os checkins' })
+  @ApiOkResponse({ description: 'Sucesso' })
   async findAll() {
     return this.checkinService.findAll();
   }
 
   @Get('/:id')
+  @ApiOperation({ summary: 'Busca check-in pelo id' })
+  @ApiOkResponse({ description: 'Check-in encontrado' })
+  @ApiNotFoundResponse({ description: 'Check-in não encontrado' })
   async findOne(@Param('id') id: string): Promise<ReturnCheckinDto> {
     const checkin = await this.checkinService.findOne(id);
     return {
@@ -52,11 +64,22 @@ export class CheckinController {
   }
 
   @Get('/key_result/:id')
+  @ApiOperation({summary: 'Busca checkins de um resultado-chave especificado pelo id'})
+  @ApiOkResponse({description: 'Sucesso'})
+  @ApiNotFoundResponse({description: 'Resultado-chave não encontrado'})
   async findCheckin(@Param('id') id: string) {
     return await this.checkinService.findKeyResult(id);
   }
 
   @Patch('/:id')
+  @ApiOperation({ summary: 'Aplica alterações parciais dos dados' })
+  @ApiOkResponse({ description: 'Check-in atualizado com sucesso!' })
+  @ApiInternalServerErrorResponse({
+    description: 'Erro ao atualizar os dados no banco de dados',
+  })
+  @ApiForbiddenResponse({
+    description: 'Você não tem autorização para acessar esse recurso',
+  })
   async updateCheckin(
     @Body(ValidationPipe) updateCheckinDto: UpdateCheckinDto,
     @GetUser() user: User,
@@ -72,6 +95,11 @@ export class CheckinController {
   }
 
   @Delete('/:id')
+  @ApiOperation({ summary: 'Exclui ckeck-in pelo id' })
+  @ApiOkResponse({ description: 'Check-in excluído com sucesso' })
+  @ApiNotFoundResponse({
+    description: 'Não foi encontrado um check-in com o ID informado',
+  })
   @Role(UserRole.MANAGER)
   async deleteCheckin(@Param('id') id: string) {
     await this.checkinService.deleteCheckin(id);
