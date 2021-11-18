ee75cfa695f75fdc5b0b6c86fec7ca4ca3b6a121
janicecaldeira <janicecaldeira@gmail.com>
2021-11-17 23:55:38 -0300

    Fix: permissão

--- src/key-results/key-results.controller.ts
+++ src/key-results/key-results.controller.ts
@@ -8,26 +8,21 @@ import {
   Param,
   Get,
   Patch,
-  ForbiddenException,
 } from '@nestjs/common';
 import { CreateKeyResultDto } from './dtos/create-key-result.dto';
 import { ReturnKeyResultDto } from './dtos/return-key-result.dto';
 import { UpdateKeyResultDto } from './dtos/update-key-result.dto';
 import { AuthGuard } from '@nestjs/passport';
-import { RolesGuard } from 'src/auth/roles.guard';
-import { Role } from '../auth/role.decorator';
-import { UserRole } from '../users/user-roles.enum';
 import { KeyResultsService } from './key-results.service';
 import { GetUser } from 'src/auth/get-user.decorator';
 import { User } from 'src/users/user.entity';
 
 @Controller('key-results')
-@UseGuards(AuthGuard(), RolesGuard)
+@UseGuards(AuthGuard())
 export class KeyResultsController {
   constructor(private keyResultsService: KeyResultsService) {}
 
   @Post()
-  @Role(UserRole.USER && UserRole.MANAGER)
   async createKeyResult(
     @Body(ValidationPipe) createKeyResultDto: CreateKeyResultDto,
   ): Promise<ReturnKeyResultDto> {
@@ -46,7 +41,6 @@ export class KeyResultsController {
   }
 
   @Get('/:id')
-  @Role(UserRole.USER)
   async findOne(@Param('id') id: string): Promise<ReturnKeyResultDto> {
     const keyResult = await this.keyResultsService.findOne(id);
     return {
@@ -61,17 +55,10 @@ export class KeyResultsController {
     @GetUser() user: User,
     @Param('id') id: string,
   ) {
-    if (user.role == UserRole.ADMIN)
-      throw new ForbiddenException(
-        'Você não tem autorização para acessar esse recurso',
-      );
-    else {
-      return this.keyResultsService.updateKeyResult(updateKeyResultDto, id);
-    }
+    return this.keyResultsService.updateKeyResult(updateKeyResultDto, id);
   }
 
   @Delete('/:id')
-  @Role(UserRole.USER)
   async deleteKeyResult(@Param('id') id: string) {
     await this.keyResultsService.deleteKeyResult(id);
     return { message: 'Resultado-chave excluído com sucesso' };
