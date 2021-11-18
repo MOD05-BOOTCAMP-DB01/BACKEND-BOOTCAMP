bce7a95d8f31fc8b8f4bb88b29b7e160f9c808cb
Priscila Miranda (fraser) <priscila.fraser@gmail.com>
2021-11-18 09:09:33 -0400

    Feat: keyresult

--- src/key-results/key-results.controller.ts
+++ src/key-results/key-results.controller.ts
@@ -8,21 +8,41 @@ import {
   Param,
   Get,
   Patch,
+  ForbiddenException,
 } from '@nestjs/common';
 import { CreateKeyResultDto } from './dtos/create-key-result.dto';
 import { ReturnKeyResultDto } from './dtos/return-key-result.dto';
 import { UpdateKeyResultDto } from './dtos/update-key-result.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from 'src/auth/roles.guard';
+import { Role } from '../auth/role.decorator';
+import { UserRole } from '../users/user-roles.enum';
 import { KeyResultsService } from './key-results.service';
 import { GetUser } from 'src/auth/get-user.decorator';
 import { User } from 'src/users/user.entity';
+import {
+  ApiCreatedResponse,
+  ApiForbiddenResponse,
+  ApiInternalServerErrorResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('Key Result')
 @Controller('key-results')
-@UseGuards(AuthGuard())
+@UseGuards(AuthGuard(), RolesGuard)
 export class KeyResultsController {
   constructor(private keyResultsService: KeyResultsService) {}
 
   @Post()
+  @Role(UserRole.USER && UserRole.MANAGER)
+  @ApiOperation({ summary: 'Cria key results' })
+  @ApiCreatedResponse({ description: 'Resultado-chave cadastrado com sucesso' })
+  @ApiInternalServerErrorResponse({
+    description: 'Erro ao salvar o resultado-chave no banco de dados',
+  })
   async createKeyResult(
     @Body(ValidationPipe) createKeyResultDto: CreateKeyResultDto,
   ): Promise<ReturnKeyResultDto> {
@@ -36,11 +56,17 @@ export class KeyResultsController {
   }
 
   @Get()
+  @ApiOperation({ summary: 'Busca todos os key results' })
+  @ApiOkResponse({ description: 'Sucesso' })
   async findAll() {
     return this.keyResultsService.findAll();
   }
 
   @Get('/:id')
+  @Role(UserRole.USER)
+  @ApiOperation({ summary: 'Busca key result pelo id' })
+  @ApiOkResponse({ description: 'Resultado-chave encontrado' })
+  @ApiNotFoundResponse({ description: 'Resultado-chave não encontrado' })
   async findOne(@Param('id') id: string): Promise<ReturnKeyResultDto> {
     const keyResult = await this.keyResultsService.findOne(id);
     return {
@@ -50,15 +76,35 @@ export class KeyResultsController {
   }
 
   @Patch('/:id')
+  @ApiOperation({ summary: 'Aplica alterações parciais dos dados' })
+  @ApiOkResponse({ description: 'Key result atualizado com sucesso!' })
+  @ApiInternalServerErrorResponse({
+    description: 'Erro ao atualizar os dados no banco de dados',
+  })
+  @ApiForbiddenResponse({
+    description: 'Você não tem autorização para acessar esse recurso',
+  })
   async updateKeyResult(
     @Body(ValidationPipe) updateKeyResultDto: UpdateKeyResultDto,
     @GetUser() user: User,
     @Param('id') id: string,
   ) {
-    return this.keyResultsService.updateKeyResult(updateKeyResultDto, id);
+    if (user.role == UserRole.ADMIN)
+      throw new ForbiddenException(
+        'Você não tem autorização para acessar esse recurso',
+      );
+    else {
+      return this.keyResultsService.updateKeyResult(updateKeyResultDto, id);
+    }
   }
 
   @Delete('/:id')
+  @Role(UserRole.USER)
+  @ApiOperation({ summary: 'Exclui key result por id' })
+  @ApiOkResponse({ description: 'Resultado-chave excluído com sucesso' })
+  @ApiNotFoundResponse({
+    description: 'Não foi encontrado um resultado-chave com o ID informado',
+  })
   async deleteKeyResult(@Param('id') id: string) {
     await this.keyResultsService.deleteKeyResult(id);
     return { message: 'Resultado-chave excluído com sucesso' };
